51988133eab7f7d81fc43ffb8b4270e36f28878c
mikecinnamon <cinndata@gmail.com>
2024-10-17 19:50:39 +0200

    Update pcos.py

--- Code/pcos.py
+++ Code/pcos.py
@@ -7,9 +7,9 @@ df = pd.read_csv(path + 'pcos.csv', index_col=0)
 
 # Exploring the data #
 df.info()
-df['pcos'].mean()
+df['pcos'].mean().round(3)
 
-# Feature matrix and target vector #
+# Target vector and features matrix #
 y = df['pcos']
 X1 = df.drop(columns=['blood', 'pcos'])
 X2 = pd.get_dummies(df['blood'])
@@ -19,9 +19,9 @@ X = pd.concat([X1, X2], axis=1)
 
 # Q1. Decision tree classifier # 
 from sklearn.tree import DecisionTreeClassifier
-clf = DecisionTreeClassifier(max_depth=5)
+clf = DecisionTreeClassifier(criterion='entropy', max_depth=5)
 clf.fit(X, y)
-clf.score(X, y).round(3)
+round(clf.score(X, y), 3)
 y_pred = clf.predict(X)
 from sklearn.metrics import confusion_matrix
 confusion_matrix(y, y_pred)
@@ -33,7 +33,7 @@ X['bmi'] = df['weight']/df['height']**2
 X['lh_fsh'] = df['lh']/df['fsh']
 X['hip_waist'] = df['hip']/df['waist']
 clf.fit(X, y)
-clf.score(X, y).round(3)
+round(clf.score(X, y), 3)
 importance = pd.Series(clf.feature_importances_, index=X.columns)
 importance[importance > 0].sort_values(ascending=False).round(3)
 
@@ -42,7 +42,20 @@ from sklearn.model_selection import cross_val_score
 cross_val_score(clf, X, y, cv=3).round(3)
 
 # Q4. Reduce the size of the decision tree # 
-clf = DecisionTreeClassifier(max_depth=4)
+clf = DecisionTreeClassifier(criterion='entropy', max_depth=4)
 clf.fit(X, y)
-clf.score(X, y).round(3)
+round(clf.score(X, y), 3)
 cross_val_score(clf, X, y, cv=3).round(3)
+clf = DecisionTreeClassifier(criterion='entropy', max_leaf_nodes=6)
+clf.fit(X, y)
+round(clf.score(X, y), 3)
+importance = pd.Series(clf.feature_importances_, index=X.columns)
+importance[importance > 0].sort_values(ascending=False).round(3)
+cross_val_score(clf, X, y, cv=3).round(3)
+
+# Plotting the tree #
+from matplotlib import pyplot as plt
+from sklearn.tree import plot_tree
+plt.figure(figsize=(13,7))
+plot_tree(clf, fontsize=10)
+plt.title('Figure 1. PCOS diagnosis tree', fontsize=16);
